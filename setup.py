e0b2ec6629ff4f22e1c8e68428f894ea49c9f45f
yu9824 <58211916+yu9824@users.noreply.github.com>
2020-11-11 16:10:34 +0900

    Update versions.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as requirements_file:
 
 setup(
     name='element_recognition',
-    version='0.0.4',
+    version='0.0.5',
     description='You can get element ratio.',
     author='yu-9824',
     author_email='yu.9824@gmail.com',
