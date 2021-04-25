258dc2e13d7be73aeb0d4efd330639c4c6e34db2
Yudai IWAMIZU <yu.9824.job@gmail.com>
2021-04-25 11:50:57 +0900

    v0.2.0 This is the version we are preparing for registration to PyPI. * Setup.py has been rewritten to enable PyPI registration. * Rewrote __init__.py to match. * MANIFEST.in was added to prepare for registration.

--- element_recognition/__init__.py
+++ element_recognition/__init__.py
@@ -1,1 +1,18 @@
+# 正規表現でtextファイルと同じやり方で読み込むのですべて手打ちする．
+
 from .element_recognition import *
+
+__version__      = '0.2.0'
+__license__      = 'MIT'
+__author__       = 'yu-9824'
+__copyright__    = 'Copyright (c) 2021 yu-9824'
+__author_email__ = 'yu.9824.job@gmail.com'
+__user_id__ = 'yu-9824'
+__app_name__ = 'element_recognition'
+__url__ = 'http://github.com/yu-9824/element_recognition/'
+
+__all__ = [
+    'element_recognition',
+    'get_ratio',
+    'make_compositions',
+    ]
