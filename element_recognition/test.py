201f200108a2d03c884be7369f4668edcc56aef9
Yudai IWAMIZU <yu.9824.job@gmail.com>
2021-04-25 12:12:41 +0900

    Add test.py to test.

---
+++ element_recognition/test.py
@@ -0,0 +1,7 @@
+from element_recognition import get_ratio, make_compositions
+
+if __name__ == '__main__':
+    products = ['Li2La2O4']
+    materials = ['Li2O', 'La2O3', 'TiO2']
+    df = get_ratio(products, materials)
+    print(df)
