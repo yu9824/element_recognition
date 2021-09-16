07978f3e048a07ae338a63d82ee5dff1034116d8
yu9824 <58211916+yu9824@users.noreply.github.com>
2021-09-16 20:02:45 +0900

    Update test

--- element_recognition/_test.py
+++ element_recognition/_test.py
@@ -1,9 +1,23 @@
-from element_recognition import get_ratio, make_compositions
-
 if __name__ == '__main__':
+    from element_recognition import get_ratio, make_compositions
+    import numpy as np
+    import pandas as pd
+
     products = ['Li2La2O4']
     materials = ['Li2O', 'La2O3', 'TiO2']
     df = get_ratio(products, materials)
+    print(df)
+
+    df = get_ratio(products[0], materials)
+    print(df)
+
+    df = get_ratio(np.array(products), materials)
+    print(df)
+
+    df = get_ratio(pd.Series(products), materials)
+    print(df)
+
+    df = get_ratio(pd.DataFrame(products), materials)
     print(df)
 
     print(make_compositions(materials))
