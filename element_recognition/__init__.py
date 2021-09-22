fd8589121051b7a32a3a5c95ef4c5443f10fedbe
yu9824 <58211916+yu9824@users.noreply.github.com>
2021-09-22 22:45:27 +0900

    v1.1.1

--- element_recognition/__init__.py
+++ element_recognition/__init__.py
@@ -2,7 +2,7 @@
 
 from .element_recognition import *
 
-__version__      = '1.1.1dev'
+__version__      = '1.1.1'
 __license__      = 'MIT'
 __author__       = 'yu9824'
 __copyright__    = 'Copyright © 2021 yu9824'
